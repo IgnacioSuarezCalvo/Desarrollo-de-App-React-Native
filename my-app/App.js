30f5c670c97ce42378be3989d42d4cc9b9d79bf5
Ignacio <igf.suarez@gmail.com>
2023-11-15 21:52:45 -0300

    cambio

--- my-app/App.js
+++ my-app/App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hola coder!</Text>
+      <Text>Soy Ignacio</Text>
+      <Text>Todavia no se que app hacer</Text>
       <StatusBar style="auto" />
     </View>
   );
