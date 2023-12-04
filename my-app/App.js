ad01c1c2a5933f4465a34dfc84c504d87f282855
Ignacio <igf.suarez@gmail.com>
2023-12-04 18:24:53 -0300

    hola

--- my-app/App.js
+++ my-app/App.js
@@ -3,9 +3,31 @@
 export default function App() {
   return (
     <View style={styles.container}>
-      <View>
+      <View style={styles.inputContainer}>
         <TextInput style={styles.input}/>
         <Button title="Add"/>
+      </View>
+      <View style={styles.listContainer}>
+        <View style={styles.cardProduct}>
+          <Text>Fernet</Text>
+          <Text>2500$</Text>
+          <Button title='DEL'/>
+        </View>
+        <View style={styles.cardProduct}>
+          <Text>Coca Cola</Text>
+          <Text>2500$</Text>
+          <Button title='DEL'/>
+        </View>
+        <View style={styles.cardProduct}>
+          <Text>Gancia</Text>
+          <Text>2500$</Text>
+          <Button title='DEL'/>
+        </View>
+        <View style={styles.cardProduct}>
+          <Text>Sprite</Text>
+          <Text>2500$</Text>
+          <Button title='DEL'/>
+        </View>
       </View>
     </View>
   );
@@ -16,9 +38,31 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'start',
+    marginTop:4,
+  },
+  inputContainer:{
+    flexDirection:"row",
+    alignSelf:"stretch",
+    justifyContent:"space-around",
+    marginTop:40,
   },
   input:{
-    
-  }
+    width:250,
+    borderWidth:2,
+    paddingHorizontal:10,
+    paddingVertical:2,
+  },
+  listContainer:{
+    width:"100%",
+    marginTop:20,
+  },
+  cardProduct:{
+    flexDirection:"row",
+    padding:10,
+    margin:10,
+    justifyContent:"space-evenly",
+    alignItems:"center",
+    borderWidth:4,
+  },
 });
